7b52ad1d1cd84ef80f8a7e80b9dee2e481cc3e8f
Rahul Rajput <RahulRaj352001@outlook.com>
2021-08-15 01:34:20 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -6,25 +6,14 @@ import "./css/style.css";
 
 export default function App(props) {
   const [title, setTitle] = useState("hello react");
-  const [isShowing, setIsShowing] = useState(false);
-  function handleClick() {
-    setIsShowing(!isShowing);
-  }
-  useEffect(() => {
   
-  },) 
+
   return (
     <section className="flex justify-center">
-      <div className="w-1/2">
+      <div className="w-10/12">
         <div className=" text-center">
           <div className="my-4">Hello{title}</div>
-          <button
-            className="p-1 bg-blue-600 text-white my-2"
-            onClick={handleClick}
-          >
-            toggle image
-          </button>
-          {isShowing === true ? <Images /> : null}
+         <Images/>
         </div>
       </div>
     </section>
--- src/components/Images.js
+++ src/components/Images.js
@@ -7,21 +7,25 @@ export default function Images() {
     "https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1334&q=80",
     "https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=668&q=80",
   ]);
-
+  const [newImgUrl, setNewImgUrl] = useState("");
   function ShowImage() {
     return Images.map((image) => {
       return (
-        <div className="w-1/3">
-          <img src={image} alt="" width="250" />
+        <div className="w-1/3 my-4">
+          <img src={image} alt="" width="150" />
         </div>
       );
     });
   }
   function handleAdd() {
-    setImages([
-      ...Images,
-      "https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1334&q=80",
-    ]);
+      if (newImgUrl !== " ") {
+        setImages([...Images, newImgUrl]);
+        setNewImgUrl("");
+    }
+  }
+ 
+  function handleChange(event) {
+    setNewImgUrl(event.target.value);
   }
   return (
     <section>
@@ -33,13 +37,18 @@ export default function Images() {
           <input
             type="text"
             className="p-2 border w-full border-gray-800 shadow rounded"
+            value={newImgUrl}
+            onChange={handleChange}
           />
         </div>
         <button
+          disabled={newImgUrl === ""}
+          className={`p-2 border ml-2 border-gray-800  ${
+            newImgUrl !== "" ? "bg-gray-400" : "bg-red-100"
+          }`}
           onClick={handleAdd}
-          className={`p-2 border bg-green-400 ml-2 border-gray-800 text-white  `}
         >
-          add{" "}
+          add
         </button>
       </div>
     </section>
